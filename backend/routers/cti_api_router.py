2b2049f36216d827539b2a2e6396f68387508b18
cremeni25 <andersonnavarro.si@gmail.com>
2026-08-13 11:45:00 -0300

    CRM: registrar sincronização móvel de itens

--- backend/routers/cti_api_router.py
+++ backend/routers/cti_api_router.py
@@ -28,6 +28,7 @@ from routers.crm_app_clientes_edicao_router import router as crm_app_clientes_ed
 from routers.crm_app_proposta_envio_router import router as crm_app_proposta_envio_router
 from routers.crm_app_oportunidade_propostas_envio_router import router as crm_app_oportunidade_propostas_envio_router
 from routers.crm_app_oportunidades_teste_router import router as crm_app_oportunidades_teste_router
+from routers.crm_app_itens_router import router as crm_app_itens_router
 
 router = APIRouter()
 router.include_router(product_catalog_router)
@@ -58,6 +59,7 @@ router.include_router(crm_app_clientes_edicao_router)
 router.include_router(crm_app_proposta_envio_router)
 router.include_router(crm_app_oportunidade_propostas_envio_router)
 router.include_router(crm_app_oportunidades_teste_router)
+router.include_router(crm_app_itens_router)
 
 
 @router.post("/upload")
