5f6b639f5b490a668fb59eb79a24578311263304
cremeni25 <andersonnavarro.si@gmail.com>
2026-08-18 23:30:32 -0300

    CTI upload: registrar router de promoção controlada

--- backend/routers/cti_api_router.py
+++ backend/routers/cti_api_router.py
@@ -34,6 +34,7 @@ from routers.crm_app_itens_router import router as crm_app_itens_router
 from routers.backoffice_fontes_router import router as backoffice_fontes_router
 from routers.backoffice_fontes_semantica_router import router as backoffice_fontes_semantica_router
 from routers.backoffice_fontes_reconciliacao_router import router as backoffice_fontes_reconciliacao_router
+from routers.backoffice_fontes_promocao_router import router as backoffice_fontes_promocao_router
 from services import ia_comercial_fontes_dinamicas as _ia_fontes_dinamicas  # noqa: F401
 
 router = APIRouter()
@@ -71,6 +72,7 @@ router.include_router(crm_app_itens_router)
 router.include_router(backoffice_fontes_router)
 router.include_router(backoffice_fontes_semantica_router)
 router.include_router(backoffice_fontes_reconciliacao_router)
+router.include_router(backoffice_fontes_promocao_router)
 
 
 @router.post("/upload")
