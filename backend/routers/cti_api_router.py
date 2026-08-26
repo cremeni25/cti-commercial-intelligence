255f05593d4e233d006749d1b138c6a8ed9f7454
cremeni25 <andersonnavarro.si@gmail.com>
2026-08-26 11:58:07 -0300

    FIX CRM: registrar rota de validação PDF sem envio

--- backend/routers/cti_api_router.py
+++ backend/routers/cti_api_router.py
@@ -27,6 +27,7 @@ from routers.ia_comercial_anexos_router import router as ia_comercial_anexos_rou
 from routers.crm_app_clientes_edicao_router import router as crm_app_clientes_edicao_router
 from routers.crm_app_cnpj_router import router as crm_app_cnpj_router
 from routers.crm_app_proposta_envio_router import router as crm_app_proposta_envio_router
+from routers.crm_app_proposta_pdf_validacao_router import router as crm_app_proposta_pdf_validacao_router
 from routers.crm_app_oportunidade_propostas_envio_router import router as crm_app_oportunidade_propostas_envio_router
 from routers.crm_app_oportunidades_teste_router import router as crm_app_oportunidades_teste_router
 from routers.crm_app_itens_router import router as crm_app_itens_router
@@ -65,6 +66,7 @@ router.include_router(ia_comercial_anexos_router)
 router.include_router(crm_app_clientes_edicao_router)
 router.include_router(crm_app_cnpj_router)
 router.include_router(crm_app_proposta_envio_router)
+router.include_router(crm_app_proposta_pdf_validacao_router)
 router.include_router(crm_app_oportunidade_propostas_envio_router)
 router.include_router(crm_app_oportunidades_teste_router)
 router.include_router(crm_app_itens_router)
